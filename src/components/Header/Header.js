6ecd2d4bdc50dc81f4b16bd1f3be270cfddd3f26
Hailey <hailey@MacBook-Pro.local>
2021-07-09 20:42:57 -0400

    styling on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,13 +13,13 @@ const Header = () => {
 <div className="right">
     <ul>
     <li>
-        <a href="/work">work</a>
+        <a href="/work">work .   </a>
         </li>
         <li>
-            <a href="/contact">contact</a>
+            <a href="/contact">contact .    </a>
         </li>
         <li>
-            <a href="/about">about</a>
+            <a href="/about">about .      </a>
         </li>
     </ul>
 </div>
